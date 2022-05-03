e7c119dd1f654abcba5616dfacde32981ae879c5
rob-by-w <rwayong.code@gmail.com>
2022-05-03 11:48:54 +0900

    Prepared cells for the mine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkmacosx import Button  # For MacOS
+from cell import *
 import settings
 import utils
 
@@ -17,6 +19,26 @@ left_frame.place(x=0, y=utils.height_prct(25))
 
 center_frame = Frame(root, bg="black", width=utils.width_prct(75), height=utils.height_prct(75))
 center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
+
+# btn1 = Button(center_frame, bg="red", text="First Button")
+# btn1 = Button(center_frame, bg="blue", text="First Button", borderless=1)  # For MacOS
+# btn1.place(x=0, y=0)
+
+# c1 = Cell()
+# c1.create_btn_object(center_frame)
+# # c1.cell_btn_object.place(x=0, y=0)
+# c1.cell_btn_object.grid(column=0, row=0)
+#
+# c2 = Cell()
+# c2.create_btn_object(center_frame)
+# # c2.cell_btn_object.place(x=40, y=0)
+# c2.cell_btn_object.grid(column=0, row=1)
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(column=x, row=y)
 
 # Run the window
 root.mainloop()
